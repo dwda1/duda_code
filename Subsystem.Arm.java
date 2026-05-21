297b264b86effb5da27b295b00b370a7840fb35f
dwda1 <dwd4aa@gmail.com>
2026-05-21 16:06:10 -0300

    Update Subsystem.Arm.java

--- Subsystem.Arm.java
+++ Subsystem.Arm.java
@@ -9,8 +9,8 @@ import com.seattlesolvers.solverslib.controller.PIDFController;
 
 
 public class Arm extends SubsystemBase {
-    private final DcMotorEx armMotor;
-    private final Servo articulationArm;
+    private DcMotorEx armMotor;
+    private Servo articulationArm;
 
     public static double kP;
     public static double kI;
@@ -22,9 +22,16 @@ public class Arm extends SubsystemBase {
     public static double TICKS_PER_REV = 537.7;
     double motorPos = armMotor.getCurrentPosition();
     double revolutions = motorPos/TICKS_PER_REV;
-    double targetRPM;
+    public static double targetRPM;
     double angle = revolutions * 360;
     double angleNormalized = angle % 360;
+
+    int armHigh = 1000; //ticks value
+    int armLow = 0;
+
+    boolean up = true;
+    double power = 0;
+
 
     public Arm(HardwareMap hwMap) {
         armMotor = hwMap.get(DcMotorEx.class, "armMotor");
@@ -34,6 +41,44 @@ public class Arm extends SubsystemBase {
         armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
+    public double armPos(int pos) {
+        articulationArm.setPosition(pos);
+        return pos;
+    }
+    public void armUP (){
+        up = true;
+    }
+    public void armDown (){
+        up = false;
+    }
+    public void OFF (){
+        up = false;
+        power = 0;
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
 
-    
-}
+    public void setTargetRPM(double rpm) {
+        targetRPM = rpm;
+    }
+    public double getTargetRPM() {
+        return targetRPM;
+    }
+
+    public double getCurrentRPM() {
+        return (armMotor.getVelocity() / TICKS_PER_REV) * 60.0;
+    }
+
+    @Override
+    public void periodic() {
+
+        pidf.setPIDF(kP, kI, kD, kF);
+
+        double currentTPS = armMotor.getVelocity();
+        double targetTPS = (currentTPS / 60.0) * TICKS_PER_REV;
+
+        if(up) {
+            double ff = targetTPS * kF;
+        }
+    }
+
+    }
