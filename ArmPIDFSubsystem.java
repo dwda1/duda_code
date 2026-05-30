5e72f9be0ca148cd75803977f547df102da766b1
dwda1 <dwd4aa@gmail.com>
2026-05-29 21:14:01 -0300

    Refactor ArmPIDFSubsystem with new constants and methods
    Refactor ArmPIDFSubsystem to include constants for positions in cm, add cmToTicks method, and adjust feedforward calculation.

--- ArmPIDFSubsystem.java
+++ ArmPIDFSubsystem.java
@@ -3,21 +3,31 @@ public class ArmPIDFSubsystem extends SubsystemBase {
   public static double kP = 0.01, kI = 0, kD = 0;
   public static double kF = 0;
   public static Controller pidf;
-  public static target = 0; 
+  public static int target = 0; 
 
   private final DcMotorEx viper;
   public double power;
 
-  public final double degrees_per_tick = 360.0 / 537.7; //grau por tick
+  public final double ticks_per_rev = 537.7;
 
-  //Posições do viper
-  public static final highChamber;
-  public static final lowChamber;
-  public static final highBasket;
-  public static final lowBasket;
-  public static final startPosition = 0;
-  public static final MAX_HEIGHT;
-  public static final MIN_HEIGHT;
+  public final double degrees_per_tick = 360.0 / 537.7; //grau por tick
+  
+  public final double cm_per_rev = 3.82 * Math.PI; // perímetro em cm (dSpool*pi) 
+  public final double cm_per_tick = cm_per_rev / ticks_per_rev; 
+
+  //Posições do viper em cm
+  public static final double highChamber = 66.0;
+  public static final double lowChamber = 33.0;
+  public static final double highBasket = 109.2;
+  public static final double lowBasket = 65.4;
+  public static final double startPosition = 0;
+  public static final double MAX_HEIGHT = 69.6;
+  public static final double MIN_HEIGHT = 0;
+
+  //conversão cm => ticks
+  public int cmToTicks(double cm) {
+    return (int) (cm / cm_per_tick);
+  } //target = cmToTicks(highBasket);
 
   //Construtor
   public ArmPIDFSubsystem(HardwareMap hwMap) {
@@ -38,21 +48,25 @@ public class ArmPIDFSubsystem extends SubsystemBase {
     int posViper = viper.getCurrentPosition();
 
     double pid = pidf.calculate(posViper, target);
-    double ff = Math.cos(Math.toRadians(target * degrees_per_tick)) * kF;
+    double ff = kF;
 
     //calcular o power
     double output = pid + ff;
     output = Math.max(-1.0, Math.min(1.0, output));
     double speed = 0.8;
     power = (speed * output);
+    viper.setPower(power);
     }
 
- //Métodos
- public 
- 
-
-
-
+  public void resetViper() {
+    pidf.reset();
+    viper.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    viper.setPower(0);
+  }
 
 
 }
+
+
+
+
