6206d2e3e0da0d6c1f744cc9423dcff7bba8fae8
dwda1 <dwd4aa@gmail.com>
2026-05-31 13:56:51 -0300

    Implement AutoViper class with command scheduling

---
+++ Auto/ViperAuto.java
@@ -0,0 +1,48 @@
+public class AutoViper extends LinearOpMode {
+
+  private ArmPIDFSubsystem viper;
+
+  @Override
+  public void runOpMode() throws InterruptedExeption {
+    viper = new ArmPIDFSubsystem(hwMap);
+    
+  ViperOff v_off = new ViperOff(viper);
+  ViperDown v_down = new ViperDown(viper);
+  GoToHighChamber v_high_chamber = new GoToHighChamber(viper);
+  GoToLowChamber v_low_chamber = new GoToLowChamber(viper);
+  GoToHighBasket v_high_basket = new GoToHighBasket(viper);
+  GoToLowBasket v_low_basket = new GoToLowBasket(viper);
+
+    waitForStart();
+
+    CommandScheduler.getInstance().schedule(
+      new SequentialCommandGroup(
+        v_low_chamber,
+        v_down,
+
+        v_high_chamber,
+        v_down,
+
+        v_low_basket,
+        v_down,
+
+        v_high_basket,
+        v_down,
+        v_off
+        )
+      );
+
+    while (opModeIsActive()) {
+
+      CommandScheduler.getInstance().run();
+
+      telemetry.addData("Viper status: ", viper.getStatus());
+      telemetry.update();
+    }
+
+    CommandScheduler.getInstance().reset();
+  }
+}
+      
+  
+  
