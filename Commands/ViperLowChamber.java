f9cf95458ba3f9a095ca10571f5f919e83abeb59
dwda1 <dwd4aa@gmail.com>
2026-05-30 22:18:04 -0300

    Create ViperLowChamber.java

---
+++ Commands/ViperLowChamber.java
@@ -0,0 +1,19 @@
+public class GoToLowChamber extends CommandBase {
+
+  private final ArmPIDFSubsystem viper;
+
+  public GoToLowChamber(ArmPIDFSubsystem subsystem) {
+    viper = subsystem;
+    addRequeriments(viper);
+  }
+
+  @Override
+  public void initialize() {
+    viper.viperToLowChamber();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
