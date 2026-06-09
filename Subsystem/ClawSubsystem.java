2a5ca0ff62f1ab84193972b450a488bcd827f4da
dwda1 <dwd4aa@gmail.com>
2026-06-09 17:21:32 -0300

    Implement ClawSubsystem for robot claw control

---
+++ Subsystem/ClawSubsystem.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import android.annotation.SuppressLint;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+public class ClawSubsystem extends SubsystemBase {
+    private Servo claw;
+    public double pos;
+
+    public enum Mode {
+        OPEN,
+        CLOSE
+    }
+    private Mode mode = Mode.OPEN;
+
+    public void Claw_Open() { mode = Mode.OPEN; }
+    public void Claw_Close() { mode = Mode.CLOSE; }
+
+    @Override
+    public void periodic() {
+        //state machine
+        switch (mode) {
+
+            case OPEN:
+                pos = 0.35;
+                claw.setPosition(pos);
+
+            case CLOSE:
+                pos = 0.0;
+                claw.setPosition(pos);
+        }
+    }
+    public String getClawStatus() {
+        return String.format(
+                "Mode=%s ClawPos=%d",
+                mode,
+                claw.getPosition()
+        );
+    }
+}
