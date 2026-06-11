2f3c278f28800dc5e700a6c57b1de93b7c30a36c
dwda1 <dwd4aa@gmail.com>
2026-06-11 15:06:57 -0300

    Add ClawOpen command to control claw subsystem

---
+++ Commands/ClawOpen.java
@@ -0,0 +1,18 @@
+public class ClawOpen extends CommandBase {
+  private final ClawSubsystem claw;
+
+  public ClawOpen (ClawSubsystem subsystem) {
+    claw = subsystem;
+    addRequirements(claw);
+  }
+
+  @Override
+  public void initialize() {
+    claw.claw_Open();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return claw.clawAtTarget();
+  }
+}
