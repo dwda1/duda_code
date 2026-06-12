c0a62bf88d4f267113cba30200172eecf458da48
dwda1 <dwd4aa@gmail.com>
2026-06-12 19:20:09 -0300

    Update JointToChamber.java

--- Commands/JointToChamber.java
+++ Commands/JointToChamber.java
@@ -1,6 +1,7 @@
 public class JointToChamber extends CommandBase {
 
   public final ArmPIDFSubsystem joint;
+  private ElapsedTime timer;
 
   public JointToChamber (ArmPIDFSubsystem subsystem) {
     joint = subsystem;
@@ -10,10 +11,11 @@ public class JointToChamber extends CommandBase {
   @Override
   public void initialize() {
     joint.jointToChamber();
+    timer = new ElapsedTime();
   }
 
   @Override
   public boolean isFinished() {
-    joint.jointAtTarget();
+    return timer.milliseconds() > 700;
   }
 }
