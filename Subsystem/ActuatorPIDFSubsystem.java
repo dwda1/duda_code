358de746f3ae8b9cfd3d1bf1c95df70461d8aec7
dwda1 <dwd4aa@gmail.com>
2026-06-13 17:11:12 -0300

    Update ActuatorPIDFSubsystem.java

--- Subsystem/ActuatorPIDFSubsystem.java
+++ Subsystem/ActuatorPIDFSubsystem.java
@@ -1,42 +1,43 @@
 public class ActuatorPIDFSubsystem extends SubsystemBase {
  
-  private DcMotorEX actuator;
+  private DcMotorEx actuator;
   public double power;
-  public static target = 0;
-  public int TOLERANCE = 15;
+  public static final double speed = 0.8;
+  public static int target = 0;
+  public static final int TOLERANCE = 15;
   
   private PIDFController pidf;
   
   public static double kP = 0, kI = 0, kD = 0;
   public static double kF = 0;
 
-  double ticks_per_rev = 537.7;
+  private static final double ticks_per_rev = 537.7;
 
   //distância percorrida por volta
-  double cm_per_rev = 4.96 * Math.PI;
+  private static final double cm_per_rev = 4.96 * Math.PI;
   
   //distancia por tick
-  double cm_per_tick = cm_per_rev / ticks_per_rev;
+  private static final double cm_per_tick = cm_per_rev / ticks_per_rev;
 
   //posições do atuator
   double MAX_HEIGHT = 
   double MIN_HEIGHT =
-  double hightChamber =
+  double highChamber =
   double lowChamber =
-  double hightBasket =
+  double highBasket =
   double lowBasket =
   double initialPosition =
 
   //convertendo cm para ticks
-  public int cmToTicks(int cm) {
-    return (int) (cm / cm_per_tick);
+  public int cmToTicks(double cm) {
+    return (int) Math.round(cm / cm_per_tick);
   }
 
-  public AtuatorPIDFSubsystem (HardwareMap hwMap) {
-    atuator = hwMap.get(DcMotor.clas, "atuator");
+  public ActuatorPIDFSubsystem (HardwareMap hwMap) {
+    actuator = hwMap.get(DcMotorEx.clas, "atuator");
 
-    atuator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    atuator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    actuator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    actuator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
     pidf = new PIDFController(kP, kI, kD, kF);
   }
@@ -49,13 +50,12 @@ public class ActuatorPIDFSubsystem extends SubsystemBase {
 
    //calcula o pid
    int posActuator = actuator.getCurrentPosition();
-   double pid = pidf.calculate(posActuator - target);
+   double pid = pidf.calculate(posActuator, target);
    double ff = kF;
    
    //aplica o pid na potencia do motor
    double output = pid + ff;
    output = Math.max(-1.0, Math.min(1.0, output));
-   double speed = 0.8;
    power = (output * speed);
    actuator.setPower(power);
   }
@@ -68,7 +68,7 @@ public class ActuatorPIDFSubsystem extends SubsystemBase {
     pidf.reset();
     actuator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     actuator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    actuator.stPower(0);
+    actuator.setPower(0);
   }
 
   //modos
@@ -77,9 +77,9 @@ public class ActuatorPIDFSubsystem extends SubsystemBase {
     OFF,
     DOWN,
     LOW_CHAMBER,
-    HIGHT_CHAMBER,
+    HIGH_CHAMBER,
     LOW_BASKET,
-    HIGHT_BASKET
+    HIGH_BASKET
   }
 
   private Mode mode = Mode.OFF;
@@ -87,9 +87,9 @@ public class ActuatorPIDFSubsystem extends SubsystemBase {
   public void actuatorOff() { mode = Mode.OFF; }
   public void actuatorDown() { mode = Mode.DOWN; }
   public void actuatorLowChamber() { mode = Mode.LOW_CHAMBER; }
-  public void actuatorHightChamber() { mode = Mode.HIGHT_CHAMBER; }
+  public void actuatorHighChamber() { mode = Mode.HIGHT_CHAMBER; }
   public void actuatorLowBasket() { mode = Mode.LOW_BASKET; }
-  public void actuatorHightBasket() { mode = Mode.HIGHT_BASKET; }
+  public void actuatorHighBasket() { mode = Mode.HIGHT_BASKET; }
 
   @Override 
   public void periodic() {
@@ -98,27 +98,27 @@ public class ActuatorPIDFSubsystem extends SubsystemBase {
    switch (mode) {
 
     case OFF:
-      resetActuator();
+      actuator.setPower(0);
       break;
 
     case DOWN:
-      target = cmToTicks(initialPOsition);
+      target = cmToTicks(initialPosition);
       break;
 
     case LOW_CHAMBER: 
       target = cmToTicks(lowChamber);
       break;
 
-    case HIGHT_CHAMBER:
-      target = cmToTicks(hightChamber);
+    case HIGH_CHAMBER:
+      target = cmToTicks(highChamber);
       break;
 
     case LOW_BASKET:
       target = cmToTicks(lowBasket);
       break;
 
-    case HIGHT_BASKET:
-      target = cmToTicks(hightBasket);
+    case HIGH_BASKET:
+      target = cmToTicks(highBasket);
       break;
    }
    
@@ -136,7 +136,7 @@ public class ActuatorPIDFSubsystem extends SubsystemBase {
 
   public String getActuatorSTATUS() {
     return String.format(
-      "Mode=%s ActuatorPos=%d ActuatorCurrentPos=%d Error=%.2f Power=%.2f Velocity=%.2f"
+      "Mode=%s Target=%d ActuatorPos=%d Error=%.2f Power=%.2f Velocity=%.2f",
      mode,
      getTarget(),
      getActuatorPosition(),
