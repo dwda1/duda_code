32dafdfc762e729c2087bc9a1e57a8bdd1f0fc6d
dwda1 <dwd4aa@gmail.com>
2026-06-15 13:56:57 -0300

    Add ActuatorOff command to turn off the actuator

---
+++ Commands/ActuatorOff.java
@@ -0,0 +1,19 @@
+public class ActuatorOff extends CommandBase {
+  
+  private final ActuatorPIDFSubsystem actuator;
+
+  public ActuatorOff(ActuatorPIDFSubsystem subsystem) {
+    actuator = subsystem;
+    addRequirements(actuator);
+  }
+
+  @Override
+  public void initialized() {
+    actuator.actuatorOff();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return actuator.atTarget();
+  }
+}
