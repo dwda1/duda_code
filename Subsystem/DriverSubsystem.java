03a15e43bf10da7adea0b6e6a8dfd2661d72354a
dwda1 <dwd4aa@gmail.com>
2026-07-06 16:29:38 -0300

    Add DriverSubsystem for motor control

---
+++ Subsystem/DriverSubsystem.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+public class DriverSubsystem extends SubsystemBase {
+
+    private DcMotorEx rightFront, rightBack, leftFront, leftBack;
+    private GoBildaPinpointDriver pinpoint;
+
+    public DriverSubsystem (HardwareMap hwMap) {
+        rightFront = hwMap.get(DcMotorEx.class, "RMF");
+        rightBack = hwMap.get(DcMotorEx.class, "RMB");
+        leftFront = hwMap.get(DcMotorEx.class, "LMF");
+        leftBack = hwMap.get(DcMotorEx.class, "LMB");
+
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+}
